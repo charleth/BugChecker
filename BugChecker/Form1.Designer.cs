9fdd8da86bc5f99857c2809dffea7e48faecbfdb
charleth <C.Gruben@gmx.de>
2021-06-08 19:02:58 +0200

    Add project files.

---
+++ BugChecker/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace BugChecker
+{
+    partial class FormBugChecker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxA = new System.Windows.Forms.CheckBox();
+            this.checkBoxB = new System.Windows.Forms.CheckBox();
+            this.checkBoxC = new System.Windows.Forms.CheckBox();
+            this.checkBoxD = new System.Windows.Forms.CheckBox();
+            this.checkBoxE = new System.Windows.Forms.CheckBox();
+            this.checkBoxF = new System.Windows.Forms.CheckBox();
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelC = new System.Windows.Forms.Label();
+            this.labelD = new System.Windows.Forms.Label();
+            this.labelE = new System.Windows.Forms.Label();
+            this.labelF = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBoxA
+            // 
+            this.checkBoxA.AutoSize = true;
+            this.checkBoxA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.checkBoxA.Location = new System.Drawing.Point(168, 42);
+            this.checkBoxA.Name = "checkBoxA";
+            this.checkBoxA.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxA.TabIndex = 0;
+            this.checkBoxA.UseVisualStyleBackColor = true;
+            this.checkBoxA.CheckedChanged += new System.EventHandler(this.checkBoxA_CheckedChanged);
+            // 
+            // checkBoxB
+            // 
+            this.checkBoxB.AutoSize = true;
+            this.checkBoxB.Location = new System.Drawing.Point(168, 90);
+            this.checkBoxB.Name = "checkBoxB";
+            this.checkBoxB.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxB.TabIndex = 1;
+            this.checkBoxB.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxC
+            // 
+            this.checkBoxC.AutoSize = true;
+            this.checkBoxC.Checked = true;
+            this.checkBoxC.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxC.Location = new System.Drawing.Point(168, 139);
+            this.checkBoxC.Name = "checkBoxC";
+            this.checkBoxC.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxC.TabIndex = 2;
+            this.checkBoxC.UseVisualStyleBackColor = true;
+            this.checkBoxC.CheckedChanged += new System.EventHandler(this.checkBoxC_CheckedChanged);
+            // 
+            // checkBoxD
+            // 
+            this.checkBoxD.AutoSize = true;
+            this.checkBoxD.Location = new System.Drawing.Point(168, 190);
+            this.checkBoxD.Name = "checkBoxD";
+            this.checkBoxD.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxD.TabIndex = 3;
+            this.checkBoxD.UseVisualStyleBackColor = true;
+            this.checkBoxD.CheckedChanged += new System.EventHandler(this.checkBoxD_CheckedChanged);
+            // 
+            // checkBoxE
+            // 
+            this.checkBoxE.AutoSize = true;
+            this.checkBoxE.Location = new System.Drawing.Point(168, 241);
+            this.checkBoxE.Name = "checkBoxE";
+            this.checkBoxE.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxE.TabIndex = 4;
+            this.checkBoxE.UseVisualStyleBackColor = true;
+            this.checkBoxE.CheckedChanged += new System.EventHandler(this.checkBoxE_CheckedChanged);
+            // 
+            // checkBoxF
+            // 
+            this.checkBoxF.AutoSize = true;
+            this.checkBoxF.Location = new System.Drawing.Point(168, 293);
+            this.checkBoxF.Name = "checkBoxF";
+            this.checkBoxF.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxF.TabIndex = 5;
+            this.checkBoxF.UseVisualStyleBackColor = true;
+            this.checkBoxF.CheckedChanged += new System.EventHandler(this.checkBoxF_CheckedChanged);
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelA.Location = new System.Drawing.Point(79, 42);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(63, 13);
+            this.labelA.TabIndex = 6;
+            this.labelA.Text = "Bug #215";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelB.Location = new System.Drawing.Point(79, 90);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(63, 13);
+            this.labelB.TabIndex = 7;
+            this.labelB.Text = "Bug #216";
+            // 
+            // labelC
+            // 
+            this.labelC.AutoSize = true;
+            this.labelC.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelC.Location = new System.Drawing.Point(79, 139);
+            this.labelC.Name = "labelC";
+            this.labelC.Size = new System.Drawing.Size(63, 13);
+            this.labelC.TabIndex = 8;
+            this.labelC.Text = "Bug #217";
+            // 
+            // labelD
+            // 
+            this.labelD.AutoSize = true;
+            this.labelD.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelD.Location = new System.Drawing.Point(79, 190);
+            this.labelD.Name = "labelD";
+            this.labelD.Size = new System.Drawing.Size(63, 13);
+            this.labelD.TabIndex = 9;
+            this.labelD.Text = "Bug #218";
+            // 
+            // labelE
+            // 
+            this.labelE.AutoSize = true;
+            this.labelE.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelE.Location = new System.Drawing.Point(79, 242);
+            this.labelE.Name = "labelE";
+            this.labelE.Size = new System.Drawing.Size(63, 13);
+            this.labelE.TabIndex = 10;
+            this.labelE.Text = "Bug #219";
+            // 
+            // labelF
+            // 
+            this.labelF.AutoSize = true;
+            this.labelF.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelF.Location = new System.Drawing.Point(79, 294);
+            this.labelF.Name = "labelF";
+            this.labelF.Size = new System.Drawing.Size(63, 13);
+            this.labelF.TabIndex = 11;
+            this.labelF.Text = "Bug #220";
+            // 
+            // FormBugChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 368);
+            this.Controls.Add(this.labelF);
+            this.Controls.Add(this.labelE);
+            this.Controls.Add(this.labelD);
+            this.Controls.Add(this.labelC);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.checkBoxF);
+            this.Controls.Add(this.checkBoxE);
+            this.Controls.Add(this.checkBoxD);
+            this.Controls.Add(this.checkBoxC);
+            this.Controls.Add(this.checkBoxB);
+            this.Controls.Add(this.checkBoxA);
+            this.Name = "FormBugChecker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BugChecker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxA;
+        private System.Windows.Forms.CheckBox checkBoxB;
+        private System.Windows.Forms.CheckBox checkBoxC;
+        private System.Windows.Forms.CheckBox checkBoxD;
+        private System.Windows.Forms.CheckBox checkBoxE;
+        private System.Windows.Forms.CheckBox checkBoxF;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.Label labelC;
+        private System.Windows.Forms.Label labelD;
+        private System.Windows.Forms.Label labelE;
+        private System.Windows.Forms.Label labelF;
+    }
+}
+
---
+++ BugChecker/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugChecker
+{
+    public partial class FormBugChecker : Form
+    {
+        public FormBugChecker()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxC_CheckedChanged(object sender, EventArgs e)
+        {
+            if
+                (checkBoxC.Checked == false)
+            {
+                checkBoxA.Checked = true;
+            }
+        }
+
+        private void checkBoxA_CheckedChanged(object sender, EventArgs e)
+        {
+            if
+                (checkBoxA.Checked == false &&
+                checkBoxD.Checked == false)
+            {
+                checkBoxC.AutoCheck = false;
+                checkBoxD.AutoCheck = false;
+
+                checkBoxC.Checked = true;
+                checkBoxD.Checked = true;
+
+                checkBoxC.AutoCheck = true;
+                checkBoxD.AutoCheck = true;
+            }
+            else if
+                (checkBoxA.Checked == false && 
+                checkBoxD.Checked == true)
+            {
+                checkBoxC.AutoCheck = false;
+                checkBoxD.AutoCheck = false;
+                checkBoxE.AutoCheck = false;
+
+                checkBoxC.Checked = false;
+                checkBoxE.Checked = true;
+                checkBoxF.Checked = true;
+
+                checkBoxC.AutoCheck = true;
+                checkBoxD.AutoCheck = true;
+                checkBoxE.AutoCheck = true;
+            }
+        }
+
+        private void checkBoxD_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void checkBoxE_CheckedChanged(object sender, EventArgs e)
+        {
+            if
+                (checkBoxE.Checked == false)
+            {
+                checkBoxC.AutoCheck = false;
+
+                checkBoxC.Checked = true;
+
+                checkBoxC.AutoCheck = true;
+            }
+        }
+
+        private void checkBoxF_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
